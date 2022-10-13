d8f31fac51564c6884ac117012e15d27e7cbade1
adilexpo <adilexpo2@gmail.com>
2022-10-13 12:29:45 +0200

    Suma

---
+++ Primera_parte.java
@@ -0,0 +1,10 @@
+public class Primera_parte {
+    public static void main(String[] args) {
+        int resultado = suma( 10, 20, 30);
+        System.out.println(resultado);
+    }
+    public static int suma(int a, int b, int c){
+        return a+b+c;
+
+    }
+}
