4c8d541e07e11076ac6cae251312db7c18c98224
AdilDev <adilexpo2@gmail.com>
2022-10-13 17:40:07 +0200

    Update Coche.java

--- Coche.java
+++ Coche.java
@@ -3,7 +3,7 @@ public class Coche {
         coche micoche = new coche();
         micoche.Aumentar();
         micoche.Aumentar();
-        System.out.println(micoche.puertas);
+        System.out.println("El número de puertas es: " + micoche.puertas);
     }
     static class coche{
         public int puertas = 3;
